c78b5f124f874f9b3cb7ac2d6688e923b60ede56
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-08 03:13:18 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Renga::Application.config.secret_key_base = '8c11d375ccbf41f55e1ee9cfdfc157defc284b0e208f84383cfdf3cc461a3fc18c58954dc7fcd9e25eb877b2752a6021c3c161f8ae12f3de53b95d59ac0d4455'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Renga::Application.config.session_store :cookie_store, key: '_renga_session'
