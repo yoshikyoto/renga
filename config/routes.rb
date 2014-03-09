296f62716d17dab7db524d2b291494876efd8292
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-09 13:23:16 +0900

    add user view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Renga::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  get "users/new"
+  match '/signup', to: 'users#new', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
