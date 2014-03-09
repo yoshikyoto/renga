8940d4395aa1b02a5d133833695f37e1a251233a
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-09 12:57:51 +0900

    refactor routing and add some link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Renga::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  # match '/help', to: 'static_pages#help', via: 'get'
-  get "static_pages/contact"
+  root "static_pages#home"
+
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
