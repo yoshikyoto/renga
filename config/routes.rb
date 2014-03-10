c393c95cb7747ea93a2c842d3b95df2869cb0571
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-10 11:09:19 +0900

    add show method to user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Renga::Application.routes.draw do
   get "users/new"
   match '/signup', to: 'users#new', via: 'get'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
