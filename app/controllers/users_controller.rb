eeaade871370b82142e8ce5325f29e8e13e0f172
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-10 13:57:39 +0900

    ユーザ登録機構完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
+# -*- coding: utf-8 -*-
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # ユーザ登録に成功した時のメッセージ
+      flash[:success] = "ユーザ登録に成功しました。Welcome to Renga World!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
