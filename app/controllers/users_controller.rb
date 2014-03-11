e8a7b944faa20437dd10be12317ede80a16a4eb1
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-11 14:17:04 +0900

    add users list(index)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
