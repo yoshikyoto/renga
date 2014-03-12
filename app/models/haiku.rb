5488b7a40f22dba0090c752fa11f676932174331
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-12 13:30:29 +0900

    show posts at user page

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 class Haiku < ActiveRecord::Base
   belongs_to :user # 関係をもたせる
-  dafault_scope -> { order('created_at DESC') }  # デフォルトを日付降順にする
-  # rails 4 では無名関数を使うらしい？ 10章要確認
-  # -> { order('created_at DESC') } みたいなラムダ関数らしい？
+  # dafault_scope -> { order('created_at DESC') }  # デフォルトを日付降順にする
+  # これは Rails 3 までで、rails 4 では無名関数を使うらしい
+  -> { order('created_at DESC') } # それがこれ
   validates :user_id, presence: true
-  validates :first_five, presence:true length: { maximum: 6 }
-  validates :middle_seven, presence:true length: { maximum: 8 }
-  validates :last_five, presence:true length: { maximum: 6 }
+  validates :first_five, presence: true, length: { maximum: 6 }
+  validates :middle_seven, presence:true, length: { maximum: 8 }
+  validates :last_five, presence:true, length: { maximum: 6 }
 end
