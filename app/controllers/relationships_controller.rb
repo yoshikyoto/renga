277de31847d0b4ffd76ebfdfcdabf82e14f449f1
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-14 16:07:57 +0900

    follow button by html

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
 class RelationshipsController < ApplicationController
+  before_action :signed_in_user
+
+  # ユーザーページにリダイレクトされるのはどうかしたほうがいいかも
   def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    redirect_to @user
   end
 
   def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    redirect_to @user
   end
 end
