31153675cc3c509a5cfef7b9262affeeb2784aee
sakamoto <sakamoto@db.soc.i.kyoto-u.ac.jp>
2014-03-14 16:39:58 +0900

    add follow button by Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,24 @@
 class RelationshipsController < ApplicationController
   before_action :signed_in_user
 
-  # ユーザーページにリダイレクトされるのはどうかしたほうがいいかも
+  # Ajax 対応版。 view/users/_follow_form.html.erb と一緒に見るといい
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    # リクエストの種類に応じて、どれかが実行される。
+    # html なら　redirect_to, js(Ajax)ならなにもしない チュートリアル11.2.5
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
